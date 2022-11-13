cdf74cf37ca44ca7771cbe0d1b50c499a5c4590f
Angshuman Agarwal <angshuman.agarwal@gmail.com>
2022-11-13 22:13:01 +0000

    Added notifications logic to showcase the case of manually opening a file. User can click on Notification to open.

--- electron-open-url/main.js
+++ electron-open-url/main.js
@@ -1,14 +1,44 @@
-const { app, BrowserWindow, shell } = require('electron')
+const { app, BrowserWindow, shell, Notification } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  })
+  
+  const mainWindow = new BrowserWindow();
 
   mainWindow.loadURL('http://localhost:8080/index.html')
+  mainWindow.maximize();
 
+function openFile(filePath){
+  shell.openPath(filePath);
+}
+  
+function shouldAutoOpen(fileName){
+  // I will write some real logic here. This check is dumb and for illustration purpose of this demo
+  return (fileName.startsWith("sample"));
+}
+
+function showNotification(fileName){
+  const options = {
+    title: `Downloaded File: ${fileName}`,
+    subtitle: 'Open file',
+    body: `Click here to open : ${fileName}`,
+    silent: false,
+    hasReply: true,  
+    timeoutType: 'never', 
+    replyPlaceholder: 'Reply Here',
+    urgency: 'critical',
+    closeButtonText: 'Close'
+}
+const customNotification = new Notification(options);
+
+customNotification.addListener('click', () => {
+  console.log('Notification is Clicked');
+  openFile(path.join(app.getPath("temp"),fileName));
+  customNotification.close();
+});
+
+customNotification.show();
+}
 
 mainWindow.webContents.session.on('will-download', (event, item, webContents) => {        
     
@@ -16,7 +46,7 @@ mainWindow.webContents.session.on('will-download', (event, item, webContents) =>
     console.log(item.getURL()); // I will check if URL is safe
     
     // once all the checks have passed, start processing
-    console.log(path.join(app.getPath("temp"),item.getFilename()));
+    console.debug(path.join(app.getPath("temp"),item.getFilename()));
     item.setSavePath(path.join(app.getPath("temp"),item.getFilename()));
 
     item.on('updated', (event, state) => {
@@ -34,8 +64,12 @@ mainWindow.webContents.session.on('will-download', (event, item, webContents) =>
     item.once('done', (event, state) => {
       if (state === 'completed') {
         console.log('Download successfully')
-        // Open the document using the external application by delegating to the shell
-        shell.openPath(item.getSavePath());
+        if(shouldAutoOpen(item.getFilename())){
+          // Open the document using the external application by delegating to the shell
+         openFile(item.getSavePath());
+        } else {
+          showNotification(item.getFilename());
+        }
       } else {
         console.log(`Download failed: ${state}`)
       }
